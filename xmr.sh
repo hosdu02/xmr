e793f16d4135f98189d8a8e4d4a6011a261f0cee
hosdu02 <84207908+hosdu02@users.noreply.github.com>
2021-05-16 03:59:05 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
 tar xf xmrig-5.11.3-xenial-x64.tar.gz
 cd xmrig-5.11.3
-./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 3NA8Z5diwFHHVzizerXbkECVdbswVKBPBa -p x -a rx/0 --keepalive --donate-level=1 --nicehash -t2 --print --randomx-mode=fast
+./xmrig -o stratum+tcp://randomxmonero.eu-west.nicehash.com:3380 -u 3NA8Z5diwFHHVzizerXbkECVdbswVKBPBa -p x -a rx/0 --keepalive --donate-level=1 --nicehash -t4 --print --randomx-mode=fast
 while [ 1 ]; do
 sleep 3
 done
